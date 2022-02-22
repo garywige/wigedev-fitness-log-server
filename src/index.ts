f6125fd4f81faa760f0141c075e5da7618736b50
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-21 16:41:41 -0800

    installed express and setup basic app

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const msg = 'Hello, World!'
-console.log(msg)
+import * as express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
