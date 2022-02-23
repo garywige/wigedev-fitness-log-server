e12b99f2e370c63a24c4f684023f8a4a4fed0152
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-23 12:53:49 -0800

    created reusable responses

---
+++ src/v1/services/responses.ts
@@ -0,0 +1,11 @@
+export class ServerMessage {
+    message: string
+
+    constructor(message: string){
+        this.message = message
+    }
+}
+
+export const BadRequestError = new ServerMessage('A parameter or request body was formatted improperly')
+export const UnauthorizedError = new ServerMessage('You are not authorized to use this interface')
+export const InternalServerError = new ServerMessage('An internal server error occurred. Please try again later.')
