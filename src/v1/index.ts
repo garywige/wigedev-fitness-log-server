4cdaa6089fb44ab464d63cba9a4ecee50eaff5ea
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-23 06:39:14 -0800

    routed exercise related interfaces

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import signin from './routes/users/signin'
 import signup from './routes/users/signup'
 import cycles from './routes/cycles/cycles'
+import exercises from './routes/exercises/exercises'
 
 const router = Router()
 
@@ -9,5 +10,6 @@ const router = Router()
 router.use('/signin', signin)
 router.use('/signup', signup)
 router.use('/cycles?', cycles)
+router.use('/exercises?', exercises)
 
 export default router
---
+++ src/v1/routes/exercises/exercises.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.get('/', async (req: Request, res: Response) => {
+    console.log('GET /v1/exercises')
+})
+
+router.post('/', async (req: Request, res: Response) => {
+    console.log('POST /v1/exercises')
+})
+
+router.get('/:id', async (req: Request, res: Response) => {
+    console.log('GET /v1/exercise/{id}')
+})
+
+router.put('/:id', async (req: Request, res: Response) => {
+    console.log('PUT /v1/exercise/{id}')
+})
+
+router.delete('/:id', async (req: Request, res: Response) => {
+    console.log('DELETE /v1/exercise/{id}')
+})
+
+export default router
