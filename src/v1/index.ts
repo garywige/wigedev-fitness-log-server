d311bcf49094f95c7f43e519d3cf0f2b1958fafe
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-22 21:18:11 -0800

    signin routing

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
+import signin from './routes/users/signin'
 
 const router = Router()
 
 // Configure all v1 routers here
+router.use('/signin', signin)
 
 export default router
