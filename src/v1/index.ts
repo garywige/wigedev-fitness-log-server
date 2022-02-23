35843a497fc8cad7ea6d1a24b8e9ea1086251164
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-23 06:29:17 -0800

    routed all cycle related interfaces

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import signin from './routes/users/signin'
 import signup from './routes/users/signup'
+import cycles from './routes/cycles/cycles'
 
 const router = Router()
 
 // Configure all v1 routers here
 router.use('/signin', signin)
 router.use('/signup', signup)
+router.use('/cycles?', cycles)
 
 export default router
---
+++ src/v1/routes/cycles/cycles.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.get('/', async (req: Request, res: Response) => {
+    console.log('GET /v1/cycles')
+})
+
+router.post('/', async (req: Request, res: Response) => {
+    console.log('POST /v1/cycles')
+})
+
+router.get('/:id', async (req: Request, res: Response) => {
+    console.log('GET /v1/cycle/{id}')
+})
+
+router.put('/:id', async (req: Request, res: Response) => {
+    console.log('PUT /v1/cycle/{id}')
+})
+
+router.delete('/:id', async (req: Request, res: Response) => {
+    console.log('DELETE /v1/cycle/{id}')
+})
+
+export default router
