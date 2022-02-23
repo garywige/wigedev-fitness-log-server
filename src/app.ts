cea44fe30341d166651be7fe4631f67078779234
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-22 20:36:21 -0800

    app.ts bootstrapping

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import * as cors from 'cors'
+import * as express from 'express'
+import * as logger from 'morgan'
+import api from './api'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(logger('dev'))
+
+app.use('/', express.static('public', {redirect: false}))
+
+app.use(api)
+
+export default app
