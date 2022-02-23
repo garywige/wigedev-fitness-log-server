0266b12c1c52d998fbfae90d3ebcb9d48633a479
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-23 06:11:37 -0800

    routed POST /v1/signup

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import signin from './routes/users/signin'
+import signup from './routes/users/signup'
 
 const router = Router()
 
 // Configure all v1 routers here
 router.use('/signin', signin)
+router.use('/signup', signup)
 
 export default router
---
+++ src/v1/routes/users/signup.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.post('/', async (req: Request, res: Response) => {
+    console.log('POST /v1/signup')
+})
+
+export default router
