b02a2547028adc13cb2856ad294c5aec19f10309
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-23 11:45:48 -0800

    workouts routes are async

--- src/v1/routes/workouts/workouts.ts
+++ src/v1/routes/workouts/workouts.ts
@@ -2,23 +2,23 @@ import { Router } from 'express'
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
     console.log('GET /v1/workouts')
 })
 
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
     console.log('POST /v1/workouts')
 })
 
-router.get('/:date', (req: Request, res: Response) => {
+router.get('/:date', async (req: Request, res: Response) => {
     console.log('GET /v1/workout/{date}')
 })
 
-router.put('/:date', (req: Request, res: Response) => {
+router.put('/:date', async (req: Request, res: Response) => {
     console.log('PUT /v1/workout/{date}')
 })
 
-router.delete('/:date', (req: Request, res: Response) => {
+router.delete('/:date', async (req: Request, res: Response) => {
     console.log('DELETE /v1/workout/{date}')
 })
 
