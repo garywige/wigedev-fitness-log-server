380ac328c35066e015447740db5ab4fc9ec66210
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-24 11:03:49 -0800

    getCycles() test

--- src/v1/services/cycleService.spec.ts
+++ src/v1/services/cycleService.spec.ts
@@ -4,12 +4,29 @@ import { Request, Response } from 'express'
 describe('CycleService', () => {
 
     let testSubject: CycleService
+    let req: Request
+    let res: Response
 
     beforeAll(() => {
         testSubject = CycleService.instance
     })
 
+    beforeEach(() => {
+
+        // create mock response
+        res = jasmine.createSpyObj('Response', ['send', 'status'])
+        res.status = jasmine.createSpy().and.returnValue(res)
+    })
+
     it('should be truthy', () => {
         expect(testSubject).toBeTruthy()
+    })
+
+    describe('getCycles()', () => {
+        it('should set response status to 200 when called', () => {
+            req = jasmine.createSpyObj('Request', [''])
+            testSubject.getCycles(req, res)
+            expect(res.status).toHaveBeenCalledWith(200)
+        })
     })
 })
