44967508f18369236bc2786d3831170d814f5ec7
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-23 17:46:39 -0800

    getExercises routing

--- src/v1/routes/exercises/exercises.ts
+++ src/v1/routes/exercises/exercises.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express'
+import { ExerciseService } from '../../services/exerciseService'
 
 const router = Router()
 
 router.get('/', async (req: Request, res: Response) => {
-    console.log('GET /v1/exercises')
+    await ExerciseService.instance.getExercises(req, res)
 })
 
 router.post('/', async (req: Request, res: Response) => {
