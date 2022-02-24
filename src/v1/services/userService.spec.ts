ca75c1dd607afb6e94f0b4ed942a4ff122375e6a
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-24 10:48:37 -0800

    postSignup() tests

--- src/v1/services/userService.spec.ts
+++ src/v1/services/userService.spec.ts
@@ -4,9 +4,18 @@ import { Request, Response} from 'express'
 describe('UserService', () => {
 
     let testSubject: UserService
+    let req: Request
+    let res: Response
 
     beforeAll(() => {
         testSubject = UserService.instance
+    })
+
+    beforeEach(() => {
+
+        // create mock response
+        res = jasmine.createSpyObj('Response', ['send', 'status'])
+        res.status = jasmine.createSpy().and.returnValue(res)
     })
 
     it('should create', () => {
@@ -14,16 +23,6 @@ describe('UserService', () => {
     })
 
     describe('postSignIn()', () => {
-
-        let req: Request
-        let res: Response
-
-        beforeEach(async () => {
-
-            // create mock response
-            res = jasmine.createSpyObj('Response', ['send', 'status'])
-            res.status = jasmine.createSpy().and.returnValue(res)
-        })
 
         it('should call status with 200 when request body is valid', () => {
             req = jasmine.createSpyObj('Request', {}, { body: { email: 'test@test.com', password: 'password'}})
@@ -34,6 +33,21 @@ describe('UserService', () => {
         it('should call status with 400 when request body is invalid', () => {
             req = jasmine.createSpyObj('Request', {}, { body: { email: 'test@test.com', bad: 'test'}})
             testSubject.postSignin(req, res)
+            expect(res.status).toHaveBeenCalledWith(400)
+        })
+    })
+
+    describe('postSignup()', () => {
+
+        it('should call status with 201 when request body is valid', () => {
+            req = jasmine.createSpyObj('Request', {}, { body: { email: 'test@test.com', password: 'password', accountType: 'free'}})
+            testSubject.postSignup(req, res)
+            expect(res.status).toHaveBeenCalledWith(201)
+        })
+
+        it('should call status with 400 when request body is invalid', () => {
+            req = jasmine.createSpyObj('Request', {}, { body: { email: 'test@test.com', bad: 'test'}})
+            testSubject.postSignup(req, res)
             expect(res.status).toHaveBeenCalledWith(400)
         })
     })
