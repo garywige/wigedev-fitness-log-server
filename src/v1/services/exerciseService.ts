082673ec57bacd69c8b23180258e99a3daf6f0fb
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-23 17:44:47 -0800

    getExercises

--- src/v1/services/exerciseService.ts
+++ src/v1/services/exerciseService.ts
@@ -1,4 +1,5 @@
-
+import { Request, Response } from "express"
+import { InternalServerError } from "./responses"
 export class ExerciseService {
     private static _instance: ExerciseService
 
@@ -12,5 +13,30 @@ export class ExerciseService {
         }
 
         return this._instance
+    }
+
+    async getExercises(req: Request, res: Response) {
+
+        // verify auth
+
+        try {
+            // business logic
+        } catch {
+            res.status(500).send(InternalServerError)
+            return
+        }
+
+        // format output
+        const output = {
+            exercises: [
+                {
+                    id: 1337,
+                    name: 'Bench Press',
+                    workoutCount: 37
+                }
+            ]
+        }
+
+        res.status(200).send(output)
     }
 }
