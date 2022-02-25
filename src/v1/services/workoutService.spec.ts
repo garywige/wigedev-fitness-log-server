29379aade72ecb7e2326c86cc971796b59565d80
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-24 16:13:10 -0800

    postWorkouts() tests

--- src/v1/services/workoutService.spec.ts
+++ src/v1/services/workoutService.spec.ts
@@ -4,7 +4,8 @@ import { WorkoutService } from "./workoutService"
 describe('WorkoutService', () => {
 
     let testSubject: WorkoutService
-    let req: Request
+    let valid: Request
+    let invalid: Request
     let res: Response
 
     beforeAll(() => {
@@ -15,5 +16,36 @@ describe('WorkoutService', () => {
 
     it('should be truthy', () => {
         expect(testSubject).toBeTruthy()
+    })
+
+    describe('getWorkouts()', () => {
+        beforeAll(() => {
+            valid = jasmine.createSpyObj('Request', {}, {query: {cycle: 1}})
+            invalid = jasmine.createSpyObj('Request', {}, {query: {invalid: 'test'}})
+        })
+
+        it('should set status 200 with valid query', () => {
+            testSubject.getWorkouts(valid, res)
+            expect(res.status).toHaveBeenCalledWith(200)
+        })
+
+        it('should set status 400 with invalid query', () => {
+            testSubject.getWorkouts(invalid, res)
+            expect(res.status).toHaveBeenCalledWith(400)
+        })
+    })
+
+    describe('postWorkouts()', () => {
+        it('should set status 201 with valid input', () => {
+            valid = jasmine.createSpyObj('Request', {}, { body: { date: '11111111', sets: ['test', 'test']}})
+            testSubject.postWorkouts(valid, res)
+            expect(res.status).toHaveBeenCalledWith(201)
+        })
+
+        it('should set status 400 with invalid input', () => {
+            invalid = jasmine.createSpyObj('Request', {}, { body: {date: '11111111', invalid: 'test'}})
+            testSubject.postWorkouts(invalid, res)
+            expect(res.status).toHaveBeenCalledWith(400)
+        })
     })
 })
