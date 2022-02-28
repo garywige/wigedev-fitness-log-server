b769f44ae5e33ebf7434e973df0f58e686f81d92
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-27 17:14:41 -0800

    fixed validation for dates

--- src/v1/services/validation.ts
+++ src/v1/services/validation.ts
@@ -4,6 +4,6 @@ export function validateInt(param: string): boolean {
 }
 
 export function validateDate(param: string): boolean {
-    const pattern = new RegExp(/^[0-9]{8}$/)
+    const pattern = new RegExp(/^20[0-9]{2}-[0-9]{2}-[0-9]{2}$/)
     return pattern.test(param)
 }
