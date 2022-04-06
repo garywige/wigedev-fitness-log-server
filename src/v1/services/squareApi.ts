1f6ac543b3626f64b729122a309ca13c3a8bd0b4
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-04-06 06:45:49 -0700

    createCustomer() refactoring

--- src/v1/services/squareApi.ts
+++ src/v1/services/squareApi.ts
@@ -3,6 +3,11 @@ import * as http from 'http'
 export class SquareApi {
 
     private httpRequest = http.request
+    private headers = {
+        'Square-Version': '2022-03-16',
+        'Authorization': `Bearer ${process.env['SQUARE_ACCESS_TOKEN']}`,
+        'Content-Type': 'application/json'
+    }
 
     private request<T>(host: string, path: string, method: string, headers?: any, body?: any) : Promise<T> {
         return new Promise<T>((resolve, reject) => {
@@ -29,16 +34,39 @@ export class SquareApi {
         })
     }
 
-    public async createCustomer(firstName: string, lastName: string, email: string): Promise<any> {
-        return this.request(process.env['SQUARE_API_URL'], '/v2/customers', 'POST', {
-            'Square-Version': '2022-03-16',
-            'Authorization': `Bearer ${process.env['SQUARE_ACCESS_TOKEN']}`,
-            'Content-Type': 'application/json'
-        },{
+    public async createCustomer(firstName: string, lastName: string, email: string): Promise<CreateCustomerOutput> {
+        return await this.request(process.env['SQUARE_API_URL'], 
+        '/v2/customers', 'POST', 
+        this.headers,{
             family_name: lastName,
             given_name: firstName,
             idempotency_key: new Date().toISOString(),
             email_address: email
         })
+    }
+}
+
+export interface CreateCustomerOutput {
+    customer: {
+        id: string,
+        address: {
+            address_line_1: string,
+            address_line_2: string,
+            administrative_district_level_1: string,
+            country: string,
+            locality: string,
+            postal_code: string
+        },
+        created_at: string,
+        creation_source: string,
+        email_address: string,
+        family_name: string,
+        given_name: string
+    },
+    errors: {
+        category: string,
+        code: string,
+        detail: string,
+        field: string
     }
 }
